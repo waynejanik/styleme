07285c6f95c6d8264e9a92dcb9bc70a0dc7f6075
Wayne A. Janik <wajanik@gmail.com>
2016-05-21 00:04:26 -0500

    changed home page

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   resources :locations
 
 
-  root 'products#index'
+  root 'pages#start'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
